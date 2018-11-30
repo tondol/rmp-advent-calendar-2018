2b64af6dadbf38daa600abacd247839b35339455
tondol <tondol@gmail.com>
2018-11-30 15:12:13 +0900

    [modify] convert to ES6

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // レンダリング開始後に ready 関数を実行。
-document.addEventListener("DOMContentLoaded", function () {
-    var scene = document.querySelector("a-scene");
+document.addEventListener('DOMContentLoaded', () => {
+    const scene = document.querySelector('a-scene');
 
     if (scene.hasLoaded) {
         ready();
     } else {
-        scene.addEventListener("renderstart", ready);
+        scene.addEventListener('renderstart', ready);
     }
 });
 
@@ -13,19 +13,19 @@ function ready() {
     console.warn("ready");
 
     function css(e, styles) {
-        for (var property in styles) {
+        for (let property in styles) {
             e.style[property] = styles[property];
         }
     }
 
-    var marquee = document.querySelector("#target .item-marquee");
+    const marquee = document.querySelector('#target .item-marquee');
 
     // フォントサイズの調整。
     css(marquee, {
-        'font-size': (640 / marquee.textContent.length) + 'px',
+        'font-size': `${640 / marquee.textContent.length}px`,
     });
 
-    var t = 0;
+    let t = 0;
 
     // render 関数を毎フレーム実行する。
     function render() {
@@ -34,9 +34,9 @@ function ready() {
         }
 
         // 要素の左マージンを変化させる (右から左に流れるアニメーションを実現) 。
-        var value = 640 * (1.0 - t * 2.0);
+        const value = 640 * (1.0 - t * 2.0);
         css(marquee, {
-            'margin-left': value + 'px',
+            'margin-left': `${value}px`,
         });
 
         t += 0.005;
